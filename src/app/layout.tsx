c7b9e9d4c29d847e2d9dab3b7af78040397f2d93
franciscogrimes <fgsieg1831@gmail.com>
2026-03-07 23:34:02 -0300

    feat: Inserção de tag google ADS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from 'next/script'
 
 
 export default function RootLayout({
@@ -11,6 +12,20 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
+
+        {/* Google Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PT5KJGJ92J"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PT5KJGJ92J');
+          `}
+        </Script>
       </body>
     </html>
   );
